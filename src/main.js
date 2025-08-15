fdd7e488b4ed842d4f0d2699155696dcec69d5c5
IVANIVALDO <142198929+IVANIVALDO@users.noreply.github.com>
2025-08-15 16:25:39 -0300

    Projeto de cards de produtos finalizado

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+import './scss/main.scss';
+
 'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const starsContainers = document.querySelectorAll('.stars');
+
+  starsContainers.forEach(container => {
+    const rating = parseInt(container.dataset.rating, 10);
+    const stars = container.querySelectorAll('.stars__star');
+
+    stars.forEach((star, index) => {
+      if(index < rating) star.classList.add('stars--active');
+    });
+  });
+});
